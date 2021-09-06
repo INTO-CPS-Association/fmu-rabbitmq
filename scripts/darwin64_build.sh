3e9d095f0a5509f2f0ced1cb532e398fdc8ab592
Casper Thule <c.thulehansen@live.com>
2021-09-06 18:36:20 +0200

    Fixed docker image for mac

--- scripts/darwin64_build.sh
+++ scripts/darwin64_build.sh
@@ -3,6 +3,7 @@ set -e
 
 target=darwin-x64
 install_name=darwin-x86_64
-dockcross_url=docker.sweng.au.dk/dockcross-darwin-x64-clang:latest
+dockcross_url=docker.sweng.au.dk/dockcross-darwin-x64-gcc:cmake3.19.0
+
 
 $(git rev-parse --show-toplevel)/scripts/build.sh $target $install_name $dockcross_url
