886f6579af309c8f05f26769621e2ee960f635e3
gitting-around <gitting-around@users.noreply.github.com>
2021-11-22 10:50:40 +0100

    removed verbose in core

--- rabbitmq-fmu/src/FmuContainer.cpp
+++ rabbitmq-fmu/src/FmuContainer.cpp
@@ -413,7 +413,7 @@ bool FmuContainer::initialize() {
     //without core logging
     //this->core = new FmuContainerCore(maxAge, calculateLookahead(lookaheadBound));
 
-    this->core->setVerbose(true);
+    this->core->setVerbose(false);
 
     if (!initializeCoreState()) {
         FmuContainer_LOG(fmi2Fatal, "logError", "Initialization failed%s", "");
